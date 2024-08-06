1177bb922f9cc322ac1d3b4d264b606038061e45
Francisco-frc <franciscofelipe.reis@gmail.com>
2024-08-06 17:09:23 -0400

    feat: addd cunstom logger

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
-import "github.com/Francisco-frc/APIsRest/router"
+import (
+	"github.com/Francisco-frc/APIsRest/config"
+	"github.com/Francisco-frc/APIsRest/router"
+)
 
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.ErrorF("config initialization error: %v", err)
+		return
+	}
+
 	// Initialize Router
 	router.Inicialize()
 }
